765905775a324eb452cd4d10c3f0160cd82343d0
zhangqi <dto-zhangqi@renruihr.com>
2025-01-17 18:24:11 +0800

    fix: 调整服务

--- service.sh
+++ service.sh
@@ -1,25 +1,46 @@
-#/bin/bash
+#!/bin/bash
+# 20250117
+# FREDZHANG
 # add to systemctl service
 
-main () {
-  local service_content="[Unit]
-  Description=Swagger Codegen Server
-  After=network.target
+main(){
+    # 安装依赖
+    npm i
+    echo "Creating service file..."
+    local SERVICE_NAME="swagger-codegen-server.service"
+    # 根据端口号，netstat，杀死进程
+    local PORT=8787
+    local PID=$(netstat -nlp | grep $PORT | awk '{print $7}' | awk -F"/" '{ print $1 }')
+    if [ -n "$PID" ]; then
+        echo "Killing process $PID..."
+        systemctl stop $SERVICE_NAME
+        kill -9 $PID
+    fi
+    local THIS_DIR=$(cd $(dirname $0); pwd)
+    echo "[Unit]
+Description=Swagger Codegen Server for generating code from Swagger API
+After=network.target
 
-  [Service]
-  Type=simple
-  WorkingDirectory=/root/swagger-codegen-server
-  ExecStart=cd /root/swagger-codegen-server && npm install && npm start
-  Restart=on-failure
+[Service]
+WorkingDirectory=$THIS_DIR
+ExecStart=npm run start
+KillMode=process
+Type=notify
 
-  [Install]
-  WantedBy=multi-user.target
-  "
-  echo "$service_content" > /etc/systemd/system/swagger-codegen-server.service
-  systemctl daemon-reload
-  systemctl enable swagger-codegen-server
-  systemctl restart swagger-codegen-server
-  systemctl status swagger-codegen-server
+[Install]
+WantedBy=multi-user.target
+Alias=swagger-codegen-server.service
+" > /etc/systemd/system/$SERVICE_NAME
+    # 重新加载 systemd 配置
+    echo "Reloading systemd daemon..."
+    systemctl daemon-reload
+
+    # 启用并启动服务
+    echo "Enabling and starting the service..."
+    systemctl enable $SERVICE_NAME
+    systemctl restart $SERVICE_NAME
+    # 检查服务状态
+    systemctl status $SERVICE_NAME
 }
 
 main
